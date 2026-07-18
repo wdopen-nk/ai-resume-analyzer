f28c0c5d4d69b975b2db611e43c83f141ad6f013
wdopen-nk <nihatsadigzada06@gmail.com>
2026-07-18 16:39:30 +0400

    Initialize FastAPI app and include resume router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 
+from app.database.init_db import init_db
+from app.api.resume import router as resume_router
+
 app = FastAPI(
     title="AI Resume Analyzer",
     version="1.0.0"
 )
 
+init_db()
+
+app.include_router(resume_router)
 
 @app.get("/")
 def root():
